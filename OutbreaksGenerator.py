af904ff90a07d359f991afbc6649d299640d5598
Pietrangelo de Biase <pvbiase@Gmail.com>
2019-12-02 14:26:53 +0100

    Need to check SA and run the code in LIACS for all 5 datasets
    Hey Victor, some good pieces of news and one bad piece of news.
    1) I finished the heurestic algorithm along with the whole experiment setup using multiprocessing and two different of tests (we can use both or decide whatever we prefer) - one calculating the score in the train set and the other in a test set (placement selected using 80% of the runs and testing in the later 20%). I believe that the test
    2) Already normalized the scores in a manner that 0 is the worst possible and 1 is the best possible
    3) The SA code is taking too much. Too much that I had to force stop the code. There was no error message but for the 1st dataset it took more than like 1 hour. So, I don't know if is something wrong...
    
    I am updating everything to github now. Please, check the .py files and try to debug the SA.
    The files works as follows:
    
    1) OutbreakGenerator.py: used only once to generate the outbreaks and is imported in other scripts because it has the function edgelist_csv_to_graph
    2) EvaluationFunctions.py: has all evaluation functions normalized in a manner that 0 is the best and 1 is the worst (so the argmins are working fine in the algorithm). No multiprocessing is being used. Each eval function is composed by two functions - one that is used to compute the metric for a run and other that uses this 1st function in all runs and take the mean
    3) Algorithms.py: here we have all the algorithms. They are (and need to be) standartized in terms of inputs and ouputs so the experiments are easier to setup. I added all you SA functions but tested only the SA_SASH that had the problem that I mentioned. The heuristic algorithm is also here using the community detection and degree centrality measures. Also, here is the function that is used to run the experiments. This is odd but is necessary because to run pool.multiprocessing the function needs to be in other script (as far as I am concerned) and, thus, the function that run the experiment is here, this function is called by the next script. There are two different experiments functions - one using the train data to calculate the performance and other using the test data.
    
    I will upload everything to github. If you can check the SA and run everything in LIACS it would be great!
    4) Experiments.py: this is the last one, it just create the data format to be inputted in the experiment function that is in the Algorithm.py script and run it.

--- OutbreaksGenerator.py
+++ OutbreaksGenerator.py
@@ -6,13 +6,12 @@ from functools import reduce
 import operator
 from random import choice
 import pickle
-from multiprocessing import Pool
-import functools
 import time
+import os
 
 ### CSV READING AND NETWORK CREATION ### 
 
-def edgelist_csv_to_graph(filename="./data/1jazz_edges.csv"):
+def edgelist_csv_to_graph(filename="./data/2tvshow_edges.csv"):
     '''
     Converts a .csv file in edgelist format to a networkX graph. 
     :param filename: relative path to the csv file
@@ -25,45 +24,7 @@ def edgelist_csv_to_graph(filename="./data/1jazz_edges.csv"):
     return G
 
 ### OUTBREAK GENERATOR ### 
-def simulate_outbreak(run, G, init_nodes=[], p=False):
-    
-    # Run the algorithm 'n_runs' times
-    initial_infected_nodes = []
-    
-    # Using a fixed p or sampling from a 1-10 distribution
-    if p==False:
-        prob = np.random.uniform(1,10,1)[0]/100
-    else:
-        prob = p
-    # Getting initial notes
-    if init_nodes==[]:
-        initial_infected_nodes = [choice(list(G.nodes()))]
-    else:
-        initial_infected_nodes = init_nodes
-
-    # Nodes that are infecting other nodes in this time step
-    transmissible_nodes = initial_infected_nodes
-    # Nodes that become infected in this time step - at start, by default, none
-    just_infected = []
-    # History of all nodes that become infected - at the start just the the initial nodes
-    all_infected = [initial_infected_nodes]
-    # The algorithm runs when there is at least one trasmissible node
-    while transmissible_nodes:
-        # For each node recently infected we are going to check its neighbors and infect new nodes with probability p
-        for n in transmissible_nodes:
-            infection = np.random.uniform(0,1,len(list(G.neighbors(n)))) < prob
-            just_infected += list(np.extract(infection, list(G.neighbors(n))))
-        # Now the recent infected become the trasmissible nodes (only if they were not infected before)
-        transmissible_nodes = list(set(just_infected) - set(reduce(operator.concat, all_infected)))
-        # And they are added to the list with the history of all nodes infected
-        all_infected.extend([transmissible_nodes])
-    # Removing the last blank element (the last element is always a blank list)
-    all_infected = all_infected[:-1]
-
-    return all_infected
-
-
-def simulate_outbreaks(G, init_nodes=[], p=False, n_runs=10):
+def simulate_outbreak_inverted_index(G, init_nodes=[], p=0.05, n_runs=10):
     '''
     Simulates an outbreak, either from a random node or a prespecified set of nodes.
     :param G: networkx graph object to use as network
@@ -72,19 +33,65 @@ def simulate_outbreaks(G, init_nodes=[], p=False, n_runs=10):
     :return all_runs_list: returns a list of lists, where each 
                             inner list is a list of infected nodes resulting from that run
     '''
-    runs = list(range(n_runs))
     
-    function = functools.partial(simulate_outbreak, G=G, init_nodes=init_nodes, p=p)
-   
-    pool = Pool(3)
-    results = pool.map(function, runs)    
-    pool.close()
-    pool.join()
-    
-    return results
+    # List with all runs output
+    all_runs_dicts = []
+    # Run the algorithm 'n_runs' times
+    for run in range(n_runs):
+        outbreak_dict = dict()
+        initial_infected_nodes = []
+        # Using a fixed p or sampling from a 20-60 distribution
+        if p==False:
+            prob = np.random.uniform(20,60,1)[0]/100
+        else:
+            prob = p
 
+        if init_nodes==[]:
+            initial_infected_nodes = [choice(list(G.nodes()))]
+            
+        else:
+            initial_infected_nodes = init_nodes
+
+        # Random seed equals to run so we always can recover the same output
+        # np.random.seed(run)
+        # Nodes that are infecting other nodes in this time step
+        transmissible_nodes = initial_infected_nodes
+        # Nodes that become infected in this time step - at start, by default, none
+        just_infected = []
+        # History of all nodes that become infected - at the start just the the initial nodes
+        [outbreak_dict.update({node:0}) for node in initial_infected_nodes]
+        # The algorithm runs when there is at least one trasmissible node
+        i = 1
+        all_infected = [initial_infected_nodes]
+        while transmissible_nodes:
+            # For each node recently infected we are going to check its neighbors and infect new nodes with probability p
+            for n in transmissible_nodes:
+                current_neighbors = list(G.neighbors(n))
+                infection = np.random.uniform(0,1,len(current_neighbors)) < prob
+                just_infected += list(np.extract(infection, current_neighbors))
+            # Now the recent infected become the trasmissible nodes (only if they were not infected before)
+            transmissible_nodes = list(set(just_infected) - set(reduce(operator.concat, all_infected)))
+            # And they are added to the list with the history of all nodes infected
+#             all_infected.extend([transmissible_nodes])
+            [outbreak_dict.update({node:i}) for node in transmissible_nodes]
+            all_infected.extend([transmissible_nodes])
+            i += 1
+        # Removing the last blank element (the last element is always a blank list)
+        all_infected = all_infected[:-1]
+        # Appending t the list with the output from all runs
+        output = (outbreak_dict, all_infected)
+        all_runs_dicts.append(output)
+
+    return all_runs_dicts
 
 ### RUNNING FOR ALL FIVE FILES### 
+
+#folders
+
+path = "C:\\Users\\pvbia\\EPA - Delft\\2o Year\\Social Network\\Final Project\\SNA_final_project (v optimized)"
+os.chdir(path)
+os.getcwd()
+
 
 def main():
     np.random.seed(1874)
@@ -93,12 +100,15 @@ def main():
                   ["3politician_outbreaks.data","./data/3politician_edges.csv"],
                   ["4public_figure_outbreaks.data","./data/4public_figure_edges.csv"],
                   ["5new_sites_outbreaks.data","./data/5new_sites_edges.csv"]
+#                  ,
+#                  ["6artist_outbreaks.data","./data/6artist_edges.csv"],
+#                  ["7livemocha_outbreaks.data","./data/7livemocha_edges.csv"]
                  ]
 
     for f in file_names:
         G = edgelist_csv_to_graph(f[1])
         t_start = time.time()
-        outbreak = simulate_outbreaks(G=G, init_nodes=[], p=0.05, n_runs=10000)
+        outbreak = simulate_outbreak_inverted_index(G=G, init_nodes=[], p=0.05, n_runs=10000)
         with open(f[0], 'wb') as filehandle:
             # store the data as binary data stream
             pickle.dump(outbreak, filehandle)
@@ -107,8 +117,3 @@ def main():
         del G
 if __name__ == '__main__':
     main()
-
-
-
-
-
